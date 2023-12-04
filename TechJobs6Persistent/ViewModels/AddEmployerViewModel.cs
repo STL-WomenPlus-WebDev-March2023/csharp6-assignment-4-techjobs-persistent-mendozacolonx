eb573e5acdd91164b8c2a28f981d6ee1dcefb280
xiomaramendoza <mendozacolonx@gmail.com>
2023-12-04 13:45:12 -0600

    Created AddEmployerViewModel and added name and location properties with validation.

---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage="Employer name is required.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Employer location is required.")]
+        public string? Location { get; set; }
+    }
+}
